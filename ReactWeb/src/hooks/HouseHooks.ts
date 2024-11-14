8bd0ac3fe43ad4d7632474cdec7ead53b913b0a3
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-14 13:16:21 -0600

    Completed

--- ReactWeb/src/hooks/HouseHooks.ts
+++ ReactWeb/src/hooks/HouseHooks.ts
@@ -1,9 +1,11 @@
 //import { useNavigate } from "react-router-dom";
 //import Problem from "../types/problem";
 import { House } from "./../types/house";
-import { useQuery} from "@tanstack/react-query";
-import axios, { AxiosError} from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import config from "../config";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import Problem from "../types/problem";
 
 
 //This is a custom hook. Basically a custom hook is a function
@@ -49,9 +51,63 @@ const useFetchHouse = (id: number) => {
   });
 };
 
+//*useAddHouse will use React Query to create HTTP POST to the API 
+//*React Query hook uses data mutation like POSTs.
+//*For add there's no cache key:  queryKey: ["houses", id],  
+//*like in useFetchHouse.
+//*Because for useMutation we don't need cache
+const useAddHouse = () => {
+  const queryClient = useQueryClient();
+  const nav = useNavigate(); //*this hookwill be used to navigate to the list of houses once POST is done successfully
+  return useMutation<AxiosResponse, AxiosError<Problem>, House>({
+    //*No cache key here instead we just write the arrow function that executes the request.
+    //*(h) means get the house instance and we post that to the API endpoint Axios
+    mutationFn: (h) => axios.post(`${config.baseApiUrl}/houses`, h), //* "h" param is the instance of the new house
+    onSuccess: () => {
+      queryClient.invalidateQueries({   //*invalidated cache using queryClient.invalidateQueries
+        queryKey: ["houses"] 
+       }); //*we know that after successful POST the cache is no longer valid and needs to be invalidated using queryClient.invalidateQueries
+     nav("/");  //*then on success we want to navigate to the list of houses
+    },
+  });
+};
+
+//*PUT and DELETE are called useMutations
+const useUpdateHouse = () => {
+  const queryClient = useQueryClient();
+  const nav = useNavigate();
+  return useMutation<AxiosResponse, AxiosError<Problem>, House>({
+    mutationFn: (h) => axios.put(`${config.baseApiUrl}/houses`, h),
+    onSuccess: (_, house) => {  //*we used underscore "_" for unused param, the second param is "house" instance
+      queryClient.invalidateQueries({   //*invalidated cache using queryClient.invalidateQueries
+        queryKey: ["houses"] 
+      });
+      nav(`/house/${house.id}`); //*navigate back to the list of houses using the id of the house
+    },
+  });
+};
+
+//*PUT and DELETE are called useMutations
+const useDeleteHouse = () => {
+  const queryClient = useQueryClient();
+  const nav = useNavigate();
+  return useMutation<AxiosResponse, AxiosError, House>({
+    mutationFn: (h) => axios.delete(`${config.baseApiUrl}/houses/${h.id}`), //*on the delete just send the house.id
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["houses"] }); //*invalidated cache using queryClient.invalidateQueries
+      nav("/");
+    },
+  });
+};
+
+//*Export the hooks
 export default useFetchHouses;
-export {useFetchHouse};
- 
+export {
+  useFetchHouse,
+  useAddHouse,
+  useUpdateHouse,
+  useDeleteHouse,
+};
   
 
 
